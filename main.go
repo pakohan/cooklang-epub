022ae0e703d7e6a3f064b1b44a85a2089958edfc
Patrick Kohan <patrick.kohan@gmail.com>
2025-04-11 20:42:03 +0200

    add action

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
+import "flag"
+
 func main() {
-	recipes, err := ParseFolder("/Users/mogli/Developer/recipes")
+	folder := flag.String("folder", ".", "Path to the folder containing recipes")
+	outputFile := flag.String("output", "recipes.epub", "Path to the output EPUB file")
+	flag.Parse()
+
+	recipes, err := ParseFolder(*folder)
 	if err != nil {
 		panic(err)
 	}
 
-	err = createBook("/Users/mogli/Desktop/out.epub", recipes)
+	err = createBook(*outputFile, recipes)
 	if err != nil {
 		panic(err)
 	}
